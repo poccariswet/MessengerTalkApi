63eda55c701ccd985faee83de702fe8a7d3a6d35
soeyusuke <s1240231@gmail.com>
2017-05-31 15:03:41 +0900

    test commit

---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+	//"strcosnv"
+)
+
+var accessToken = os.Getenv("ACCESS_TOKEN")
+var verifyToken = os.Getenv("VERIFY_TOKEN")
+
+// const ...
+const (
+	EndPoint = "https://graph.facebook.com/v2.6/me/messages"
+)
+
+// ReceivedMessage ...
+type ReceivedMessage struct {
+	Object string  `json:"object"`
+	Entry  []Entry `json:"entry"`
+}
+
+// Entry ...
+type Entry struct {
+	ID        string       `json:"id"`
+	Time      int          `json:"time"`
+	Messaging []Messaging  `json:"messaging"`
+}
+
+// Messaging ...
+type Messaging struct {
+	Sender    Sender    `json:"sender"`
+	Recipient Recipient `json:"recipient"`
+	Timestamp int       `json:"timestamp"`
+	Message   Message   `json:"message"`
+}
+
+// Sender ...
+type Sender struct {
+	ID string `json:"id"`
+}
+
+// Recipient ...
+type Recipient struct {
+	ID string `json:"id"`
+}
+
+// Message ...
+type Message struct {
+	MID  string `json:"mid"`
+	Seq  int  `json:"seq"`
+	Text string `json:"text"`
+}
+
+// SendMessage ...
+type SendMessage struct {
+	Recipient Recipient `json:"recipient"`
+	Message struct {
+		Text string `json:"text"`
+	} `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/", TopPageHandler)
+	http.HandleFunc("/webhook", webhookHandler)
+	port := os.Getenv("PORT")
+	address := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(address, nil)
+}
+
+func TopPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is go-bot application's top page.")
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		verifyTokenAction(w, r)
+	}
+	if r.Method == "POST" {
+		webhookPostAction(w, r)
+	}
+}
+
+func verifyTokenAction(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("hub.verify_token") == verifyToken {
+		log.Print("verify token success.")
+		fmt.Fprintf(w, r.URL.Query().Get("hub.challenge"))
+	} else {
+		log.Print("Error: verify token failed.")
+		fmt.Fprintf(w, "Error, wrong validation token")
+	}
+}
+
+func webhookPostAction(w http.ResponseWriter, r *http.Request) {
+	var receivedMessage ReceivedMessage
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Print(err)
+	}
+	if err = json.Unmarshal(body, &receivedMessage); err != nil {
+		log.Print(err)
+	}
+	messagingEvents := receivedMessage.Entry[0].Messaging
+	for _, event := range messagingEvents {
+		senderID := event.Sender.ID
+		if &event.Message != nil && event.Message.Text != "" {
+			sendTextMessage(senderID, event.Message.Text)
+		}
+	}
+	fmt.Fprintf(w, "Success")
+}
+
+func sendTextMessage(senderID string, text string) {
+	recipient := new(Recipient)
+	recipient.ID = senderID
+	m := new(SendMessage)
+	m.Recipient = *recipient
+	m.Message.Text = text
+
+	log.Print("-----------------------------------")
+	log.Print(m.Message.Text)
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		log.Print(err)
+	}
+
+	req, err := http.NewRequest("POST", EndPoint, bytes.NewBuffer(b))
+	if err != nil {
+		log.Print(err)
+	}
+
+	values := url.Values{}
+	values.Add("access_token", accessToken)
+	req.URL.RawQuery = values.Encode()
+	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+	client := &http.Client{Timeout: time.Duration(30 * time.Second)}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Print(err)
+	}
+
+	defer res.Body.Close()
+	var result map[string]interface{}
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Print(err)
+	}
+	log.Print(result)
+}
